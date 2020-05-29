b87c71c756921a8885520d8e782f984535c5265b
anunverse <60874725+anunverse@users.noreply.github.com>
2020-05-29 23:27:21 +0800

    Create 奇数码问题.cpp

---
+++ 题目/奇数码问题.cpp
@@ -0,0 +1,59 @@
+//https://www.acwing.com/problem/content/description/110/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define ll long long 
+
+const int N = 250010;
+ll a[N], tmp[N], b[500][500], c[500][500], d[N];
+int n;
+ll res;
+
+//求逆序对数量
+ll merge_sort(ll a[], int l, int r)
+{
+    if(l>=r) return res;
+    int mid = (l + r)>>1;
+    merge_sort(a,l,mid);
+    merge_sort(a,mid+1,r);
+    int i = l, j = mid + 1, k = 0;
+    while(i<=mid && j<=r)
+    {
+        if(a[i]<=a[j]) tmp[k++] = a[i++];
+        else tmp[k++] = a[j++], res += mid - i + 1;
+    }
+    while(i<=mid) tmp[k++] = a[i++];
+    while(j<=r) tmp[k++] = a[j++];
+    for(int i=l,j=0;i<=r;i++,j++) a[i] = tmp[j];
+    return res;
+}
+
+int main()
+{
+    while (cin >> n)
+    {
+        ll k = 0;
+        for (int i = 1; i <= n; i ++)
+            for (int j = 1; j <= n; j ++)
+            {
+                cin >> b[i][j];
+                if (b[i][j]) a[++k] = b[i][j];
+            }
+        k = 0;
+        for (int i = 1; i <= n; i ++)
+            for (int j = 1; j <= n; j ++)
+            {
+                cin >> c[i][j];
+                if (c[i][j]) d[++k] = c[i][j];
+            }
+        res = 0;
+        ll ans1 = merge_sort(a, 1, k);
+        res = 0;
+        ll ans2 = merge_sort(d, 1, k);
+        if ((ans1 ^ ans2) & 1) cout << "NIE" << endl;
+        else cout << "TAK" << endl;
+    }
+    return 0;
+}
